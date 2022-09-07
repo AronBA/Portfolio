a13fab4076ddb9389932c1c785fd822c4b7974b7
AronBA <72823328+AronBA@users.noreply.github.com>
2022-09-07 14:56:28 +0200

    added contact formular added top to page button

---
+++ static/js/scripts.js
@@ -0,0 +1,12 @@
+
+const scrollTop = document.getElementById('topbutton')
+window.onload = () => {
+    scrollTop.style.opacity = 0;
+}
+window.onscroll = () => {
+    if (window.scrollY > 200) {
+        scrollTop.style.opacity = 1;
+    } else {
+        scrollTop.style.opacity = 0;
+    }
+};
